d2f6f5d2692060259de3cb0aea1847313c52c855
Luke Palmer <lrpalmer@gmail.com>
2017-03-29 00:51:05 -0700

    WE HAVE LOOP!

--- Source/Looper.hs
+++ Source/Looper.hs
@@ -2,13 +2,13 @@
 
 module Looper where
 
+import qualified Data.Array.IO as Array
 import qualified Data.Time.Clock as Clock
 import qualified Foreign.Ptr as Foreign
 import qualified Foreign.Marshal.Array as Foreign
 import qualified System.MIDI as MIDI
 
-import Control.Monad (forM_, filterM)
-import System.IO (withFile, IOMode(AppendMode), hPutStrLn)
+import Control.Monad (filterM, (<=<), forM_)
 
 import Data.IORef
 import Data.Word
@@ -25,25 +25,11 @@ foreign export ccall hs_looper_main
     -> IO ()
 foreign export ccall hs_looper_exit :: StablePtr LooperState -> IO ()
 
-data MarshalMidiMessage = MarshalMidiMessage {
-    mmmSamplePosition :: Word32,
-    mmmByte1 :: Word8,
-    mmmByte2 :: Word8,
-    mmmByte3 :: Word8 }
-
-instance Storable MarshalMidiMessage where
-    alignment _ = 4
-    sizeOf _    = 8
-    peek ptr  = MarshalMidiMessage <$> peekByteOff ptr 0 <*> peekByteOff ptr 4 <*> peekByteOff ptr 5 <*> peekByteOff ptr 6
-    poke ptr (MarshalMidiMessage{..}) = do
-        pokeByteOff ptr 0 mmmSamplePosition
-        pokeByteOff ptr 4 mmmByte1
-        pokeByteOff ptr 5 mmmByte2
-        pokeByteOff ptr 6 mmmByte3
-
 data LooperState = LooperState 
     { lsMidiSource :: MIDI.Connection
     , lsMidiDest   :: MIDI.Connection
+    , lsLoop       :: Array.IOUArray Int Float
+    , lsIndex      :: IORef Int
     }
 
 findConnection :: (MIDI.MIDIHasName a) => String -> [a] -> IO a
@@ -55,20 +41,27 @@ findConnection name = fmap head . filterM (\c -> (name ==) <$> MIDI.getName c)
 hs_looper_init = do
     source <- flip MIDI.openSource Nothing =<< findConnection "APC40 mkII" =<< MIDI.enumerateSources
     dest <- MIDI.openDestination =<< findConnection "APC40 mkII" =<< MIDI.enumerateDestinations
+    array <- Array.newArray (0,44099) 0
+    index <- newIORef 0
     MIDI.start source
-    newStablePtr $ LooperState source dest
+    newStablePtr $ LooperState source dest array index
 hs_looper_main state window input output channels = do
-    {-
-    withFile "/tmp/looperlog" AppendMode $ \h -> do
-        forM_ [0..fromIntegral midiMessages-1] $ \i -> do
-            msg <- peekElemOff midiData i
-            hPutStrLn h $ "  " ++ show (mmmSamplePosition msg) ++ " " ++ show (mmmByte1 msg) ++ " " ++ show (mmmByte2 msg) ++ " " ++ show (mmmByte3 msg)
-    -}
-    LooperState src dest <- deRefStablePtr state
+    LooperState src dest loop ixref <- deRefStablePtr state
     time <- floor . realToFrac . Clock.utctDayTime <$> Clock.getCurrentTime
     MIDI.send dest $ MIDI.MidiMessage 0 (MIDI.NoteOn 32 (fromIntegral (time `mod` 128)))
+
+    loopix0 <- readIORef ixref
+    forM_ [0..fromIntegral window-1] $ \i -> do
+        let loopix = (loopix0+i) `mod` 44100
+        cur <- Array.readArray loop loopix
+        buf <- peekElemOff channels 0
+        inp <- peekElemOff buf i
+        let new = cur*0.9 + inp
+        Array.writeArray loop loopix new
+        pokeElemOff buf i new
+    modifyIORef ixref (+ fromIntegral window)
 hs_looper_exit state = do
-    LooperState src dest <- deRefStablePtr state
+    LooperState src dest _ _ <- deRefStablePtr state
     MIDI.stop src
     MIDI.close src
     MIDI.close dest
